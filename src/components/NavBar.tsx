d055948ec9071a98fa3a7ea516c8791e81e74412
CoderKumarS <100927807+CoderKumarS@users.noreply.github.com>
2024-09-14 19:12:26 +0530

    Working on  the Home Page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
 const NavBar = () => {
+  const [search, setSearch] = useState("");
   return (
     <nav className="bg-gray-800 sticky top-0">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="flex items-center justify-start">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 ">
+        <div className="relative flex items-center justify-evenly h-16">
+          <div className="basis-1/12 flex items-center content-start">
+            <img
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+              alt="Logo"
+              className="h-8 w-8"
+            />
+          </div>
+          <div className="basis-7/12 flex items-center justify-center">
             <div className="md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
+              <div className="ml-10 flex items-center content-center space-x-4">
                 <NavLink
                   to="/"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium no-underline"
@@ -40,6 +49,19 @@ const NavBar = () => {
                 </NavLink>
               </div>
             </div>
+          </div>
+          <div className="basis-1/3 flex items-center content-end">
+            <Form className="rounded container-sm">
+              <Form.Group controlId="email">
+                <Form.Control
+                  type="text"
+                  placeholder="Type to Search"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+              </Form.Group>
+            </Form>
+            <Button>Search</Button>
           </div>
         </div>
       </div>
